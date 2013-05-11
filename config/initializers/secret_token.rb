66c0a142aa0b927f018dfe61cb434305e98c3b16
shobhab shrestha <shobhabstha@gmail.com>
2013-05-11 08:21:24 +0545

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hack3san::Application.config.secret_token = '561f610f32da4725fb0fcc676e39bf108a518bf467ec9ada20d6a75175d3a4bb7590893d52b65f0bcf2c8566ca50c0db7be6cfbed6ee1c2993cfbf8f57e4246e'
